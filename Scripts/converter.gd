1f4485ab5259a602df19eeddf2f82d8ee129a0df
aoomUwU <119989552+aoomUwU@users.noreply.github.com>
2023-11-18 19:32:46 +0700

    Update converter.gd

--- Scripts/converter.gd
+++ Scripts/converter.gd
@@ -3,11 +3,21 @@ class_name Converter
 
 #10 -> 2
 static func ConvertBase10To2Base(arg: String) -> String:
-	return arg;
+	var num = arg.to_int()
+	var binary = ""
+	while num > 0:
+		binary = str(num % 2) + binary
+		num = num / 2
+	return binary
 
 #10 -> 16
 static func ConverBase10To16Base(arg: String) -> String:
-	return arg;
+	var num = arg.to_int()
+	var hex = ""
+	while num > 0:
+		hex = str("0123456789ABCDEF"[num % 16]) + hex
+		num = num / 16
+	return hex
 
 #2 -> 10
 static func ConverBase2To10Base(arg: String) -> String:
